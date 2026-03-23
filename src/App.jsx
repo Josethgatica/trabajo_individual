abce5af2be778968cfc4cb653b5328a76165d3ed
Joseth Gatica <josethgatica@example.com>
2026-03-23 15:58:27 -0600

    confi

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RutaProtegida from './componets/rutas/RutaProtegida';
+import Inicio from "./views/Inicio";
+import Categorias from "./views/Categorias";
+import Catalogo from "./views/Catalogo";
+import Productos from "./views/Productos";
+import Login from "./views/Login";
+import Pagina404 from "./views/Pagina404";
 
-import './App.css'
 
-function App()  {
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Trabajo individual</h1>
-    </div>
-  )
+    <Router>
+      <Encabezado />
+
+      <main className="margen-superior-main">
+        <Routes> {/* Ruta pública */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Rutas protegidas */}
+          <Route 
+            path="/" 
+            element={<RutaProtegida><Inicio /></RutaProtegida>} 
+          />
+          <Route 
+            path="/categorias" 
+            element={<RutaProtegida><Categorias /></RutaProtegida>} 
+          />
+          
+          {/* Ruta pública / catálogo */}
+          <Route path="/catalogo" element={<Catalogo />} />
+          
+          <Route 
+            path="/productos" 
+            element={<RutaProtegida><Productos /></RutaProtegida>} 
+          />
+
+          {/* Manejo de error 404 */}
+          <Route path="*" element={<Pagina404 />} />
+        </Routes>
+      </main>
+    </Router>
+  );
 }
 
-export default App
+export default App;
